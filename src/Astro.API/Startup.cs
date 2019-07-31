d96f497bb870ae33ce0116e936b748558a949edb
Phil <phil.grujovski@gmail.com>
2019-07-31 22:30:53 +1000

    Added DBUP and ruleset/stylecop

--- src/Astro.API/Startup.cs
+++ src/Astro.API/Startup.cs
@@ -25,6 +25,8 @@ namespace Astro.API
             {
                 c.SwaggerDoc("v1", new Info { Title = "Astro API", Version = "v1" });
             });
+
+            services.AddCors(c => c.AddPolicy("AllowOrigin", options => options.AllowAnyOrigin()));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -48,6 +50,8 @@ namespace Astro.API
 
             app.UseHttpsRedirection();
             app.UseMvc();
+
+            app.UseCors(builder => builder.AllowAnyOrigin());
         }
     }
 }
---
+++ src/Astro.DbUp/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using DbUp;
+using Microsoft.Extensions.Configuration;
+using static System.Console;
+
+namespace Astro.DbUp
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+
+            var connString = config.GetConnectionString("Astro");
+
+            EnsureDatabase.For.SqlDatabase(connString);
+
+            var upgrader = DeployChanges
+                .To
+                .SqlDatabase(connString)
+                .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
+                .LogToConsole()
+                .Build();
+
+            var result = upgrader.PerformUpgrade();
+
+            if (!result.Successful)
+            {
+                ForegroundColor = ConsoleColor.Red;
+                WriteLine(result.Error);
+                ResetColor();
+
+#if DEBUG
+                ReadKey();
+#endif
+                return;
+            }
+
+            ForegroundColor = ConsoleColor.Green;
+            WriteLine("Success!");
+            ResetColor();
+
+#if DEBUG
+            ReadKey();
+#endif
+        }
+    }
+}
