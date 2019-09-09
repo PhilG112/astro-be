c7d3347109166c5be318670e42b9253097df645d
Philg112 <phil.grujovski@gmail.com>
2019-09-09 14:29:10 +1000

    Update

--- src/Astro.API/Application/Request/Post/LoginRequestModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Astro.API.Application.Request.Post
-{
-    public class LogInRequestModel
-    {
-        public string UserName { get; set; }
-
-        public string Password { get; set; }
-    }
-}
