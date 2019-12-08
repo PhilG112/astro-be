6d5e0388d58644139df3c7428816d385c194a485
Phillip Grujovski <phil.grujovski@gmail.com>
2019-12-08 20:30:02 +1100

    Version bump to .net core 3.1

--- src/Astro.API/Startup.cs
+++ src/Astro.API/Startup.cs
@@ -1,21 +1,14 @@
-﻿using System;
-using System.Linq;
-using System.Text;
-using Astro.API.Application.Auth;
+﻿using System.Text;
+using System.Text.Json.Serialization;
 using Astro.API.Application.Services.Upload;
-using Astro.API.Application.Stores.Celestial;
 using FluentValidation.AspNetCore;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Tokens;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Converters;
-using Swashbuckle.AspNetCore.Swagger;
 
 namespace Astro.API
 {
@@ -37,11 +30,9 @@ namespace Astro.API
                     fv.RegisterValidatorsFromAssemblyContaining<Startup>();
                     fv.RunDefaultMvcValidationAfterFluentValidationExecutes = false;
                 })
-                .SetCompatibilityVersion(CompatibilityVersion.Version_2_2)
                 .AddJsonOptions(options =>
                 {
-                    options.SerializerSettings.Converters.Add(new StringEnumConverter());
-                    options.SerializerSettings.NullValueHandling = NullValueHandling.Include;
+                    options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
                 });
 
             services.AddAuthentication(options =>
@@ -72,24 +63,21 @@ namespace Astro.API
             services.AddSingleton<IUploadService, UploadService>();
         }
 
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
-            {
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
 
+            app.UseHttpsRedirection();
+            app.UseRouting();
             app.UseSwaggerConfig();
             app.UseHttpsRedirection();
             app.UseCors(options => options.WithOrigins("http://localhost:4200").AllowAnyMethod().AllowAnyHeader());
             app.UseAuthentication();
-            app.UseMvc();
             app.UseBlobStorageClient();
+            app.UseEndpoints(config => config.MapControllers());
         }
 
         protected virtual void ConfigureTestServices(IServiceCollection services)
