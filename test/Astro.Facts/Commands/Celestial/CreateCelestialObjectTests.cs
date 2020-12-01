9cca1554cfa0d65d5bcfd1800f8303fa564c6294
Philg112 <pgrujovski@protonmail.com>
2020-12-01 13:18:17 +1100

    tests

---
+++ test/Astro.Facts/Commands/Celestial/CreateCelestialObjectTests.cs
@@ -0,0 +1,56 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Astro.Abstractions.Data;
+using Astro.Application.Celestial.Commands.CreateCelestialObject;
+using Astro.Application.Celestial.Dtos;
+using FluentAssertions;
+using Moq;
+using Xunit;
+
+namespace Astro.Facts.Commands.Celestial
+{
+    public class CreateCelestialObjectTests
+    {
+        [Fact]
+        public async Task GivenValidObject_WhenHandling_ThenReturnCorrectResult()
+        {
+            var handler = GetHandler();
+
+            var result = await handler.Handle(GetCommand(), CancellationToken.None);
+
+            result.Should().Be(1);
+        }
+
+        private CreateCelestialObjectCommandHandler GetHandler(
+            Mock<ISqlDataRepository> dataRepoMock = null)
+        {
+            if (dataRepoMock is null)
+            {
+                dataRepoMock = new Mock<ISqlDataRepository>();
+
+                dataRepoMock.Setup(x => x.QueryFirstAsync<int>(It.IsAny<string>(), It.IsAny<object>()))
+                .ReturnsAsync(1);
+            }
+
+            return new CreateCelestialObjectCommandHandler(dataRepoMock.Object);
+        }
+
+        private CreateCelestialObjectCommand GetCommand()
+        {
+            return new CreateCelestialObjectCommand
+            {
+                Name = "name",
+                AbsoluteMagnitude = 10,
+                Description = "description",
+                Designation1 = "d1",
+                Designation2 = "d2",
+                Designation3 = "d3",
+                Designation4 = "d4",
+                Distance = 11,
+                DistanceTolerance = 1,
+                Magnitude = 20,
+                ObjectType = ObjectTypeDto.Galaxy
+            };
+        }
+    }
+}
---
+++ test/Astro.Facts/Validators/Celestial/CreateCelestialObjectValidatorTests.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Astro.Application.Celestial.Commands.CreateCelestialObject;
+using Astro.Application.Celestial.Dtos;
+using Xunit;
+using FluentValidation.TestHelper;
+
+namespace Astro.Facts.Validators.Celestial
+{
+    public class CreateCelestialObjectValidatorTests
+    {
+        private readonly CreateCelestialObjectValidator _validator;
+
+        public CreateCelestialObjectValidatorTests()
+        {
+            _validator = new CreateCelestialObjectValidator();
+        }
+
+        [Fact]
+        public void GivenValidObject_WhenValidating_ThenShouldNotHaveValidationError()
+        {
+            var command = new CreateCelestialObjectCommand
+            {
+                Name = "name",
+                AbsoluteMagnitude = 10,
+                Description = "description",
+                Designation1 = "d1",
+                Designation2 = "d2",
+                Designation3 = "d3",
+                Designation4 = "d4",
+                Distance = 11,
+                DistanceTolerance = 1,
+                Magnitude = 20,
+                ObjectType = ObjectTypeDto.Galaxy
+            };
+
+            var result = _validator.TestValidate(command);
+
+            result.ShouldNotHaveAnyValidationErrors();
+        }
+
+        [Fact]
+        public void GivenInvalidName_WhenValidating_ThenShouldHaveError()
+        {
+            var command = new CreateCelestialObjectCommand
+            {
+                Name = "somereallystupidlongassnamethatis40+chars"
+            };
+
+            var result = _validator.TestValidate(command);
+
+            result.ShouldHaveValidationErrorFor(x => x.Name);
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        [InlineData("somereallystupidlongassnamekjhkjthatisallaroundtheworld40+chars")]
+        public void GivenInvalidDesignation1_WhenValidating_ThenShouldHaveError(string deg1)
+        {
+            var command = new CreateCelestialObjectCommand
+            {
+                Designation1 = deg1,
+            };
+
+            var result = _validator.TestValidate(command);
+
+            result.ShouldHaveValidationErrorFor(x => x.Designation1);
+        }
+    }
+}
